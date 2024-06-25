a56a59f5133f8ee594a7325c21799a218880fcb9
JustCati <ale.catinello@gmail.com>
2024-06-25 12:24:49 +0200

    feat: argparse

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import time
+import argparse
+import datetime
+
+from src.utils.utils import getDevice, fix_random_seed
+
+
+
+
+
+def main(args):
+    path = args.path
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Path {path} does not exist")
+
+    path = os.path.join(path, "images")
+    if not os.path.exists(path):
+        raise ValueError(f"Path {path} does not exist")
+
+    modelOutputPath = ""
+    if args.train:
+        modelOutputPath = os.path.join(os.getcwd(), "ckpts", "sammask_rcnn_" + str(datetime.datetime.fromtimestamp(int(time.time()))))
+        if not os.path.exists(modelOutputPath):
+            os.makedirs(modelOutputPath)
+    elif args.resume:
+        modelOutputPath = args.resume
+    elif args.perf or args.eval or args.demo:
+        if args.perf:
+            modelOutputPath = args.perf
+        elif args.eval:
+            modelOutputPath = args.eval
+        elif args.demo:
+            modelOutputPath = args.demo
+
+    if not os.path.exists(modelOutputPath) and not args.sample:
+        raise ValueError(f"Path {modelOutputPath} does not exist")
+
+    
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="EgoObject Istance Segmentation with SAMASK-RCNN")
+    parser.add_argument("--path", type=str, default=os.path.join(os.getcwd(), "data", "EgoObjects"), help="Path to the data directory")
+    parser.add_argument("--sample", action="store_true", default=False, help="Plot a sample image from the dataset with ground truth masks")
+    parser.add_argument("--train", action="store_true", default=False, help="Force Training of the model")
+    parser.add_argument("--demo", type=str, default="", help="Run a demo of inference on 3 random image from the validation set with the model at the specified path")
+    parser.add_argument("--perf", type=str, default="", help="Plot the performance of the model at the specified path")
+    parser.add_argument("--eval", type=str, default="", help="Evaluate the model at the specified path")
+    parser.add_argument("--save", action="store_true", default=False, help="Save the demo images to the model directory")
+    parser.add_argument("--resume", type=str, default="", help="Resume training from the specified model path")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to train the model")
+    args = parser.parse_args()
+
+    main(args)
