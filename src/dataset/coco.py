80f209d01b5265d361f69e5cbd2bfd57f9ee7637
JustCati <ale.catinello@gmail.com>
2024-07-05 17:17:22 +0200

    fix: swap & append in single call

--- src/dataset/coco.py
+++ src/dataset/coco.py
@@ -1,5 +1,27 @@
+import os
 import json
 from lvis import LVIS
+
+
+
+def swap_categories_ids(json_path):
+    with open(json_path, "r") as f:
+        data = json.load(f)
+    for i in range(len(data["annotations"])):
+        data["annotations"][i]["category_id"] = data["annotations"][i]["_category_id"]
+    with open(json_path.replace(".json", "_fixed.json"), "w") as f:
+        json.dump(data, f)
+    return json_path.replace(".json", "_fixed.json")
+
+
+def append_metadata(json_path, metadata_json_path):
+    with open(json_path, "r") as f:
+        data = json.load(f)
+    with open(metadata_json_path, "r") as f:
+        metadata = json.load(f)
+    data["categories"] = metadata["categories"]
+    with open(json_path, "w") as f:
+        json.dump(data, f)
 
 
 
@@ -8,7 +30,16 @@ def convert_to_coco(src_json_path, metadata_json_path, dst_path):
     with open(metadata_json_path, "r") as f:
         lvis_metadata = json.load(f)
 
-    lvis = LVIS(src_json_path)
+    # Copy _category_id in category_id for each annotation
+    print("Swapping categories ids...")
+    new_path = swap_categories_ids(src_json_path)
+
+    # Append categories ids to metadata
+    print("Appending categories ids to metadata...")
+    append_metadata(new_path, metadata_json_path)
+
+
+    lvis = LVIS(new_path)
     coco = {
         "images": [],
         "annotations": [],
@@ -16,7 +47,11 @@ def convert_to_coco(src_json_path, metadata_json_path, dst_path):
     }
 
     # Copy categories
-    coco["categories"] = lvis_metadata["categories"]
+    for cat in lvis.load_cats(None):
+        coco["categories"].append({
+            "id": cat["id"],
+            "name": cat["name"]
+        })
     print("Categories: ", len(coco["categories"]))
     
     # Copy annotations
@@ -43,5 +78,6 @@ def convert_to_coco(src_json_path, metadata_json_path, dst_path):
     print("Images: ", len(coco["images"]))
     print()
 
+    os.remove(new_path)
     with open(dst_path, "w") as f:
         json.dump(coco, f)
