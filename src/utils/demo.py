704222144bb28fb4b8653118ceaa829e76207acf
JustCati <ale.catinello@gmail.com>
2024-07-31 12:56:42 +0200

    fix: remove useless bbox score selection

--- src/utils/demo.py
+++ src/utils/demo.py
@@ -2,15 +2,12 @@ import torch
 
 
 
-def demo(model, img, target, MASK_THRESHOLD, BBOX_THRESHOLD, device):
+def demo(model, img, target, MASK_THRESHOLD, device):
     model.eval()
     with torch.no_grad():
         prediction = model([img.to(device)])
         prediction = {k: v.to("cpu") for k, v in prediction[0].items()}
 
-    #! Thresholding for visualization (PlotDemo shows only the pixels with 1)
-    for i in range(len(prediction['boxes'])):
-        prediction['scores'][i] = prediction['scores'][i] > BBOX_THRESHOLD
     for i in range(len(prediction["masks"])):
         prediction["masks"][i] = prediction["masks"][i] > MASK_THRESHOLD
 
